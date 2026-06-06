15d05e56c857bd2cc0f567e1dbc2acdbe85c2886
ifelp <iranildofpersonal18@gmail.com>
2026-06-06 19:02:53 -0300

    feat(main): setting a minimal server mainfile that returns a simple message on localhost:3001.

---
+++ api/src/server.ts
@@ -0,0 +1,21 @@
+import Express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = Express();
+const port = process.env.PORT || 3001;
+
+app.use(Express.json());
+app.use(cors({
+    origin: ["*"] //enquanto ainda não temos os serviços bem definidos.
+}))
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({
+        status: 200,
+        message: "Feito com <3 no Cin."
+    });
+})
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+})
