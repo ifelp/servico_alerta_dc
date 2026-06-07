6c79e542b0d8892b36aa0b8e06b972cf4e26d297
ifelp <iranildofpersonal18@gmail.com>
2026-06-07 01:40:38 -0300

    feat(user controller): adding user get methods fot http requests.

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -5,7 +5,7 @@ import { UserService } from "@services";
 export class UserController{
 
     static async create(req: Request, res: Response){
-        try{
+        try {
             const dto: CreateUserDTO = req.body;
             const { name, email, rawPassword } = dto;
 
@@ -19,11 +19,42 @@ export class UserController{
                 message: "Usuário cadastrado com sucesso.",
                 user
             });
+
         } catch(error: any) {
             console.error(error);
             return res.status(400).json({
                 message: "Erro durante cadastro de usuário.",
+                error: error.message
             })
+        }
+    }
+
+    static async getAllUsers(res: Response){
+        try{
+            const users = await UserService.getAll();
+
+            return res.status(200).json(users);
+
+        } catch(error: any) {
+            console.error(error);
+            return res.status(500).send();
+        }
+    }
+
+    static async getUserById(req: Request, res: Response){
+        try{
+            const { id } = req.query;
+            if(Number.isNaN(id)) throw new Error("Id do usuário inválido.");
+
+            const user = await UserService.getById(Number(id));
+
+            if(!user) return res.status(204).send();
+            return res.status(200).json(user);
+            
+        } catch (error: any) {
+            console.error(error)
+            if(error.message == "Id do usuário inválido.") return res.status(400).json({ error: error.message });
+            return res.status(500).send();
         }
     }
 
