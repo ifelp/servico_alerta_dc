378fc4d924c1458175b34c12ea819e3892f7fd87
ifelp <iranildofpersonal18@gmail.com>
2026-06-07 00:49:35 -0300

    feat(user service): adding update method in service.

--- api/src/services/userService.ts
+++ api/src/services/userService.ts
@@ -1,3 +1,4 @@
+import { dbClient } from "@config";
 import { UserModel } from "@models";
 import { CreateUserDTO, UserResponseDTO, UpdateUserDTO } from "@types";
 import bcrypt from "bcryptjs";
@@ -43,6 +44,29 @@ export class UserService{
         const user = await UserModel.findById(id);
 
         return user;
+    }
+
+    static async update(id: number, data: UpdateUserDTO): Promise<boolean> {
+
+        const userExists = await UserModel.findById(id);
+        if(!userExists) throw new Error("O usuário não existe no banco de dados.");
+
+        const dataToUpdate: any = {}
+
+        if(data.name) dataToUpdate.name = data.name;
+        if(data.email) {
+            const emailExists = await UserModel.findByEmail(data.email);
+            if(emailExists) throw new Error("E-mail informado pelo usuário já está em uso.");
+            dataToUpdate.email = data.email;
+        }
+        if(data.rawPassword){
+            const hashedPassword = await bcrypt.hash(data.rawPassword, saltRounds);
+            dataToUpdate.password_hash = hashedPassword;
+        }
+
+        const result = await UserModel.update(id, dataToUpdate);
+
+        return result;
     }
 
 }
