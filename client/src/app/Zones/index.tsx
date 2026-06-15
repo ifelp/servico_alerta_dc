78078db27d71e7e4810b6228fd6e99bebe33e14a
ifelp <iranildofpersonal18@gmail.com>
2026-06-15 12:38:53 -0300

    feat(zone page): adding zone page wrapped in page wrapper component.

---
+++ client/src/app/Zones/index.tsx
@@ -0,0 +1,10 @@
+import PageWrapper from "../../components/pageWrapper";
+import Zones from "./ClientComponents";
+
+export default function ZonePage(){
+    return (
+        <PageWrapper showHeader={true}>
+            <Zones/>
+        </PageWrapper>
+    )
+}
