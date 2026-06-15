8588801ef33082fc37c01d7e8b9ee7c5d4d402dd
ifelp <iranildofpersonal18@gmail.com>
2026-06-15 12:39:32 -0300

    feat(router): adding zone page into app router spec.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './app/Home'
+import ZonePage from './app/Zones'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>
+  },
+  {
+    path: '/zona',
+    element: <ZonePage/>
   }
 ])
 
