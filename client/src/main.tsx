d44dc7b8d303f99c1ea1004c14737e95e23f90f9
ifelp <iranildofpersonal18@gmail.com>
2026-06-15 16:42:34 -0300

    chore(provider): Wrapping application in zone provider.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ZoneProvider } from './contexts/zoneContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ZoneProvider>
+      <App />
+    </ZoneProvider>
   </StrictMode>,
 )
