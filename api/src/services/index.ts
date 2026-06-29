0149bddbaf371f5f3280ebba2e7152cf6491e56f
ifelp <iranildofpersonal18@gmail.com>
2026-06-28 22:30:11 -0300

    feat(zone): adding zone service to validate zone rules.

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,7 +1,9 @@
 import { UserService } from "./userService";
 import { AlertService } from "./alertService";
+import { ZoneService } from "./zoneService";
 
 export {
     UserService,
-    AlertService
+    AlertService,
+    ZoneService
 }
